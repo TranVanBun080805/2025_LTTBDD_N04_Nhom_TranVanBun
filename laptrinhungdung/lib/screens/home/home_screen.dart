57df9fe795f27340ceaf6a9fcfcbc7c5f5375266
Tranvanbun080805 <23010370@st.phenikaa-uni.edu.vn>
2025-10-31 20:36:59 +0700

    create home_screen

--- laptrinhungdung/lib/screens/home/home_screen.dart
+++ laptrinhungdung/lib/screens/home/home_screen.dart
@@ -1,27 +1,77 @@
 import 'package:flutter/material.dart';
+import '../../widgets/navbar.dart';
 
-class homeScreen extends StatelessWidget {
-  const homeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() =>
+      _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = const [
+    Center(
+      child: Text(
+        'Trang chủ - Quản lý chi tiêu!',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ),
+    Center(child: Text('Thống kê chi tiêu')),
+    Center(child: Text('Trang cá nhân')),
+    Center(child: Text('Cài đặt ứng dụng')),
+  ];
+
+  void handleAdd() {
+    ScaffoldMessenger.of(
+      context,
+    ).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.only(
+          bottom: 20,
+          left: 16,
+          right: 16,
+        ),
+        content: const Text(
+          "Thêm giao dịch mới!",
+        ),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Navbar(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromARGB(
+          255,
+          191,
+          195,
+          214,
+        ),
         title: const Text(
           'Trang quản lý chi tiêu',
-        ),
-        backgroundColor: const Color(0xFF1E2A78),
-      ),
-      body: const Center(
-        child: Text(
-          'Chào mừng bạn đến với ứng dụng quản lý chi tiêu!',
-
           style: TextStyle(
-            fontSize: 20,
+            fontSize: 24,
             fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
         ),
       ),
+      body: pages[currentIndex],
+      currentIndex: currentIndex,
+      onTabSelected: (index) =>
+          setState(() => currentIndex = index),
+      onAddPressed: handleAdd,
     );
   }
 }
