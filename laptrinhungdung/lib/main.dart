3e6d2bcaeb8c27edd76fc4354f4e5438dccbdad2
Tranvanbun080805 <23010370@st.phenikaa-uni.edu.vn>
2025-11-06 10:16:50 +0700

    update main

--- laptrinhungdung/lib/main.dart
+++ laptrinhungdung/lib/main.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
-import 'screens/auth/login_screen.dart';
-import 'utils/colors.dart';
-import 'screens/auth/AuthWelcomeScreen.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
+import 'controllers/incomeProvider.dart';
+import 'controllers/categoryProvider.dart';
 import 'screens/load.dart';
+import 'utils/colors.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('vi', 'VN'),
+      ],
+      path:
+          'lib/translations', // ✅ đường dẫn chứa file ngôn ngữ
+      fallbackLocale: const Locale(
+        'vi',
+        'VN',
+      ), // 🔹 Ngôn ngữ mặc định
+      startLocale: const Locale(
+        'vi',
+        'VN',
+      ), // 🔹 App khởi động với Tiếng Việt
+      saveLocale:
+          true, // 🔹 Ghi nhớ lựa chọn của người dùng
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,16 +38,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Quan ly chi tieu',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: kPrimaryColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Incomeprovider(),
         ),
-        useMaterial3: true,
+        ChangeNotifierProvider(
+          create: (_) => CategoryProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'app_title'.tr(),
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: kPrimaryColor,
+          ),
+          scaffoldBackgroundColor: const Color(
+            0xFFF5F7FA,
+          ),
+        ),
+        localizationsDelegates:
+            context.localizationDelegates,
+        supportedLocales:
+            context.supportedLocales,
+        locale: context.locale,
+        home: const LoadingScreen(),
       ),
-      home: const LoadingScreen(),
     );
   }
 }
